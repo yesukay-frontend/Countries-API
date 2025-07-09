ea814c216acaa138e03aa6e5a20e5d5bfd9d7c8e
Yesukay <yesukay2@gmail.com>
2025-07-09 08:52:24 -0400

    Add empty state

--- src/app/Components/country-card/country-card.component.ts
+++ src/app/Components/country-card/country-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Country } from '../../Model/country.interface';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-country-card',
@@ -9,5 +10,10 @@ import { CommonModule } from '@angular/common';
   styleUrl: './country-card.component.scss',
 })
 export class CountryCardComponent {
+  constructor(private router: Router) {}
   @Input() country!: Country;
+
+  openCountryDetails() {
+    this.router.navigate(['country', this.country.name.common]);
+  }
 }
--- src/app/Pages/country-details-page/country-details-page.component.ts
+++ src/app/Pages/country-details-page/country-details-page.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Country } from '../../Model/country.interface';
+import { loadCountryByCode } from '../../State/Countries/countries.actions';
+import { selectSelectedCountry } from '../../State/Countries/countries.selectors';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-country-details-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './country-details-page.component.html',
-  styleUrl: './country-details-page.component.scss'
+  styleUrl: './country-details-page.component.scss',
 })
-export class CountryDetailsPageComponent {
+export class CountryDetailsPageComponent implements OnInit {
+  country$!: Observable<Country | null>;
 
+  constructor(private store: Store, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const code = this.route.snapshot.paramMap.get('code');
+    if (code) {
+      this.store.dispatch(loadCountryByCode({ code }));
+    }
+
+    this.country$ = this.store.select(selectSelectedCountry);
+  }
+
+  getNativeName(country: Country): string {
+    return country.name.nativeName
+      ? Object.values(country.name.nativeName)[0]?.common || ''
+      : '';
+  }
+
+  getCurrencies(country: Country): string {
+    return country.currencies
+      ? Object.values(country.currencies)
+          .map((currency) => currency.name)
+          .join(', ')
+      : '';
+  }
+
+  getLanguages(country: Country): string {
+    return country.languages ? Object.values(country.languages).join(', ') : '';
+  }
 }
